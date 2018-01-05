2c35128254a5674b57c7cea4c270a4a5c4197445
Shawn Eion Smith <ses44@psu.edu>
2018-01-05 08:35:55 -0500

    Adding test cases

--- src/test/java/edu/psu/swe/ssml/AlexaSpeachBuilderTests.java
+++ src/test/java/edu/psu/swe/ssml/AlexaSpeachBuilderTests.java
@@ -3,6 +3,7 @@ package edu.psu.swe.ssml;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.equalTo;
 
+import java.time.DayOfWeek;
 import java.time.LocalDate;
 import java.time.LocalTime;
 
@@ -54,6 +55,21 @@ public class AlexaSpeachBuilderTests {
                                .say("Know what you can do?")
                                .sayAs(SsmlSayAsType.EXPLETIVE, "Smile")
                                .say("widely")
+                               .ssml();
+    
+    System.out.println(ssml);
+    //assertThat(ssml, equalTo("<speak>Can I tell you a secret? <amazon:effect name=\"whispered\">secret</amazon:effect></speak>"));
+  }
+  
+  @Test
+  public void testWithMultiples() {
+    String ssml = AlexaSpeachBuilder.builder()
+                               .say("Know what the day and time is?")
+                               .weekday(DayOfWeek.THURSDAY)
+                               .pause(BreakStrength.MEDIUM)
+                               .date(LocalDate.now(), SSMLDateFormat.DAY_MONTH_YEAR)
+                               .pause(BreakStrength.MEDIUM)
+                               .time(LocalTime.now())
                                .ssml();
     
     System.out.println(ssml);
